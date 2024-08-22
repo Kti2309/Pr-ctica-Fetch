56407724b2a475aaf1d844f12343aca1f76c77d2
Kti2309 <Cotinegreyra104@gmail.com>
2024-08-22 10:22:15 -0300

    commit

--- Ejercicio fetch y json/js/script.js
+++ Ejercicio fetch y json/js/script.js
@@ -1,7 +1,7 @@
 // En este archivo no utilizamos el evento "DOMContentLoaded", ya que se colocó el atributo "defer" en la importación del script,
 // que nos soluciona el problema de los elementos no cargados del DOM. Más info => https://www.w3schools.com/tags/att_script_defer.asp
 
-const DATA_URL = "data.json"; // URL que contiene los datos que queremos mostrar
+const DATA_URL = "json/data.json"; // URL que contiene los datos que queremos mostrar
 
 const container = document.getElementById("container"); // "Traemos" utilizando el DOM el div de id "container" para colocar la información en él
 
@@ -19,7 +19,7 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
-fetch('DATA_URL')
+fetch(DATA_URL)
 .then( response =>{
   if(!response.ok){
     throw new Error("No disponible");
@@ -27,4 +27,10 @@ fetch('DATA_URL')
   return response.json();
 
 })
+.then(function (data){
+  console.log(data)
+  showData(data.students)
+})
+  
+
 
